9364d06d152b9696ab3034a4d83c6d76cbddb83e
Joni Halttunen <joni.halttunen@gmail.com>
2019-11-02 18:38:52 +0200

    magneetti hiottu, kamera liikkuu ja pelaaja sen mukana

---
+++ Assets/Scripts-Joni/EnemyMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+	public GameObject playerObject;
+	public float speed = 1f;
+    public bool attached = false;
+    
+    void Start()
+    {
+		playerObject = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        if (!attached) {
+            var playerPosition = playerObject.transform.position;
+            transform.position = Vector3.MoveTowards(transform.position, playerPosition, speed);
+        }
+	}
+
+	public void PickUp(Transform newParent) {
+		transform.SetParent(newParent);
+        attached = true;
+        gameObject.GetComponent<CircleCollider2D>().isTrigger = true;
+	}
+
+	void OnTriggerEnter2D(Collider2D c) {
+        if (attached) { return; }
+        print("JOO");
+		Transform t = c.gameObject.transform;
+		PickUp(t);
+	}
+}
---
+++ Assets/Scripts-Joni/PlayerMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+	public float maxX = 10f;
+	public float maxY = 10f;
+	public float speed = 5f;
+    public Vector2 localpos;
+    public Transform cam;
+    public Vector2 clampPos;
+
+	void FixedUpdate() {
+        //var newPosition = transform.position;
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+        	localpos += Vector2.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            localpos += Vector2.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            localpos += Vector2.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            localpos += Vector2.down * speed * Time.deltaTime;
+        }
+        //newPosition.y = Mathf.Clamp(newPosition.y, -maxY, maxY);
+        //newPosition.x = Mathf.Clamp(newPosition.x, -maxX, maxX);
+        //transform.position = newPosition;
+        localpos = Vector2.Min(clampPos, localpos);
+        localpos = Vector2.Max(-clampPos, localpos);
+        var newPos = cam.position + (Vector3)localpos;
+        newPos.z = 0;
+        transform.position = newPos;
+	}
+
+    void Start() {
+        var c = cam.GetComponent<Camera>();
+        clampPos = new Vector2(c.aspect * c.orthographicSize, c.orthographicSize);
+    }
+}
